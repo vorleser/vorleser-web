8cc5d8a588869a7071dc93a8ad557fb5a3efbaf4
Hans Ole Hatzel <hhatzel@gmail.com>
2017-10-03 10:57:20 +0200

    Remove node stuff

--- src/api/util.ts
+++
@@ -1,6 +0,0 @@
-import { baseUrl } from "config";
-
-async function login(url: string) {
-    let response = await fetch(`${window.location.protocol}//${baseUrl}${url}`)
-}
-
--- src/config.tsx
+++
@@ -1,9 +0,0 @@
-interface Config {
-    baseUrl: string
-}
-
-let config: Config = {
-    baseUrl: "localhost:5000"
-}
-
-export = config
