70a106a7c630a86c80e9832a37f786f3057376c0
Hans Ole Hatzel <hhatzel@gmail.com>
2017-12-01 21:14:16 +0100

    Remove unneeded config value

--- src/Config.elm
+++ src/Config.elm
@@ -5,9 +5,6 @@ name = "Vorleser"
 baseUrl : String
 baseUrl = "http://localhost:8000/"
 
-baseUrlData : String
-baseUrlData = "http://localhost:8000/datasadfasd"
-
 -- interval in which to upload playstates
 playstateUploadInterval : Float
 playstateUploadInterval = 10
